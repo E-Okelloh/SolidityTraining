13fffc561223ab464c1c9aa22089b5d4916f9d0a
E-Okelloh <eouma359@gmail.com>
2025-04-13 02:19:22 +0300

    hello welcome to solidity

---
+++ contracts/DataTypes.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+contract SimpleStorage {
+
+    /** 
+    *solidity data types
+    *bool
+    *unsigned integer (uint)
+    *Address(address)
+    *int
+    *Bytes
+    *string
+    */
+//functions
+uint256 number;
+
+}
